7d5d021b17d2d82f7ab2afe261cd90018ec668ac
Jiaqi <blacker0lily311@gmail.com>
2022-09-10 13:18:39 +1000

    test

--- test_frontend/map.js
+++ test_frontend/map.js
@@ -2,20 +2,17 @@ import createGlobe from 'cobe'
 
 const globe = createGlobe(canvas, options)
 
-markers: [
-    { location: [37.7595, -122.4367], size: 0.03 },
-    { location: [40.7128, -74.006], size: 0.1 }
-]
 
-let currentPhi = 0
 
-const globe = createGlobe(canvas, {
-  onRender: (state) => {
-    // Called on every animation frame.
-    // `state` will be an empty object, return updated options.
-    state.phi = currentPhi
+// Drop down button functions
 
-    // Rotate the globe by 0.01 on every frame.
-    currentPhi += 0.01
-  },
-})
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("myBtn").click();
+  }
+});
